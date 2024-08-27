32c0ae974a890416f8eb48740e2acff416e9675e
unknown <hichamsskouti@gmail.com>
2024-08-27 17:30:56 +0200

    dog section

--- my-app/src/components/ProductCard/ProductCard.jsx
+++ my-app/src/components/ProductCard/ProductCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import "../ProductCard/ProductCard.css"
-function ProductCard() {
+function ProductCard({ imag ,
+    desc, 
+    gene,
+    age,
+    prix}) {
   return (
-    <div>ProductCard</div>
+    <div>
+        <img src={imag} alt='dog'></img>
+        <h3>{desc}</h3>
+        <div>
+            <p>Gene: <span>{gene}</span></p>
+            <p>Age <span>{age}</span> Months</p>
+        </div>
+        <h3>{prix}<span>VND</span></h3>
+    </div>
   )
 }
 
--- my-app/src/pages/HomePage.jsx
+++ my-app/src/pages/HomePage.jsx
@@ -3,6 +3,69 @@ import "../styles/HomePage.css";
 import Header from "../components/Header/Header";
 import HeroSection from "../components/HeroSection/HeroSection";
 import TitleSection from "../components/TitleSection/TitleSection";
+import dog1 from "../assets/dog1.png"
+import dog2 from "../assets/dog2.png"
+import dog3 from "../assets/dog3.png"
+import dog4 from "../assets/dog4.png"
+import dog5 from "../assets/dog5.png"
+import dog6 from "../assets/dog6.png"
+import dog7 from "../assets/dog7.png"
+import dog8 from "../assets/dog8.png"
+import ProductCard from "../components/ProductCard/ProductCard"
+
+
+const dummyDogData  = [
+  {
+    imag:dog1,
+    desc:"MO231 - Pomeranian White",
+    gene:'Male',
+    age:2,
+    prix:69000000
+  },{
+    imag:dog2,
+    desc:"MO502 - Poodle  Tiny Yellow",
+    gene:'Female',
+    age:2,
+    prix:39000000
+  },{
+    imag:dog3,
+    desc:"MO102 - Poodle  Tiny Sepia",
+    gene:'Male',
+    age:2,
+    prix:40000000
+  },{
+    imag:dog4,
+    desc:"MO512 - Alaskan  Malamute Grey",
+    gene:'Male',
+    age:2,
+    prix:89000000
+  },{
+    imag:dog5,
+    desc:"MO231 - Pembroke  Corgi Cream",
+    gene:'Male',
+    age:2,
+    prix:79000000
+  },{
+    imag:dog6,
+    desc:"MO502 - Pembroke  Corgi Tricolor",
+    gene:'Female',
+    age:2,
+    prix:90000000
+  },{
+    imag:dog7,
+    desc:"MO231 - Pomeranian  White",
+    gene:'Female',
+    age:2,
+    prix:65000000
+  },{
+    imag:dog8,
+    desc:"MO512 - Poodle  Tiny Dairy Cow",
+    gene:'Male',
+    age:2,
+    prix:50000000
+  }
+
+]
 
 function HomePage() {
   return (
@@ -16,6 +79,19 @@ function HomePage() {
         </div>
         <div className="container">
         <TitleSection Title="Whats new?" Text="Take A Look At Some Of Our Pets" ButtonText="View more"></TitleSection>
+        <div>
+
+        {
+          dummyDogData.map((item,index)=>(
+            <ProductCard imag={item.imag}
+            desc = {item.desc} 
+            gene={item.gene}
+            age={item.age}
+            prix ={item.prix}
+            ></ProductCard>
+          ))
+        }
+        </div>
         </div>
     </div>
   );
