ae45ec49d3e7df65e2848c579da6d2957d406fef
unknown <hichamsskouti@gmail.com>
2024-10-08 17:05:03 +0200

    server setup

---
+++ server/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+//const validator = require('validator');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Please provide your email!'],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      //minlength: 8,
+      //select: false
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+require('dotenv').config({ path: '.env.local' });
+const mongoose = require('mongoose');
 
 
 
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
+mongoose.connect(DB, {}).then(() =>{
+    console.log('Database connection successful')
+     })
+    .catch((err) => console.log('Database connection error:', err))
 
 
 const httpServer = require('http').createServer(app);
