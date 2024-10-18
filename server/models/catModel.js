d8df4c8f4e58a6779c8a8facd040b00c3d3fbb6c
unknown <hichamsskouti@gmail.com>
2024-10-18 15:23:27 +0200

    cat model

---
+++ server/models/catModel.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+//const validator = require('validator');
+
+const catSchema = new mongoose.Schema(
+  {
+    
+    name: {
+        type: String,
+        required: true
+    },
+    sku: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    vaccinated: {
+        type: Boolean,
+        required: true
+    },
+    dewormed: {
+        type: Boolean,
+        required: true
+    },
+    certified: {
+        type: Boolean,
+        required: true
+    },
+    microchip: {
+        type: Boolean,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Sold', 'Available'], 
+        default: 'Available'
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female'], 
+        default: 'Available'
+    },
+    birthDate: {
+        type: Date,
+        required:true
+    },
+    location: {
+        type: String,
+        required:true
+    },
+    additionalInfo: {
+        type: [String],
+        default: []
+    },
+    images: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Image' }],
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+
+const Cat = mongoose.model('Cat', catSchema);
+
+module.exports = Cat;
